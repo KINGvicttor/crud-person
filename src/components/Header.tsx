04ae12bd69b1fb759b17fb2b7d91b9cf6e6fdb08
João Victor <victorking0820@gmail.com>
2025-04-11 12:55:11 -0300

    header frontend done

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import returnIcon from "../assets/img/return-icon.png"
+import notificationIcon from "../assets/img/notification-icon.png"
+
+export const Header = () => {
+    return(
+        <div className="w-[calc(100vw-270px)] h-[60px] flex justify-between bg-primary-blue ">
+            <div className="w-[800px] flex items-center">
+                <a href=""><img className="w-[20px] h-[20px] ml-10" src={returnIcon.src} /></a>
+            </div>
+            <div className="w-[300px] flex items-center">
+                <input 
+                    className="px-3 py-1 mr-2 border border-secondary-white rounded-lg placeholder:text-sm placeholder:text-secondary-white"
+                    type="text" 
+                    name=""
+                    placeholder="Search"/>
+                <img className="w-[20px] h-[20px]" src={notificationIcon.src}/>
+            </div>
+        </div>
+    )
+}
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./Header"
+
+
 export const Home = () => {
-    return(
+    return (
         <div className="w-[calc(100vw-270px)] h-screen bg-primary-blue">
-            
+            <Header />
+            <div className="w-[calc(100vw-270px)] h-[calc(100vh-60px)]">
+
+            </div>
         </div>
     )
 }
