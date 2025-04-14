24088bc2e6e02f1d549b3273c0b8ffb9324df131
João Victor <victorking0820@gmail.com>
2025-04-14 10:39:24 -0300

    home page frontend done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
             </div>
             <div className="w-[300px] flex items-center">
                 <input 
-                    className="px-3 py-1 mr-2 border border-secondary-white rounded-lg placeholder:text-sm placeholder:text-secondary-white"
+                    className="px-3 py-1 mr-2 border-2 border-secondary-white rounded-lg placeholder:text-sm placeholder:text-secondary-white"
                     type="text" 
                     name=""
                     placeholder="Search"/>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,8 @@
 import { Header } from "./Header"
+import courseIcon from "../assets/img/course-icon.png"
+import studentIcon from "../assets/img/student-icon.png"
+import paymentIcon from "../assets/img/payment-icon.png"
+import reportIcon from "../assets/img/report-icon.png"
 
 
 export const Home = () => {
@@ -6,7 +10,42 @@ export const Home = () => {
         <div className="w-[calc(100vw-270px)] h-screen bg-primary-blue">
             <Header />
             <div className="w-[calc(100vw-270px)] h-[calc(100vh-60px)]">
-
+                <div className="container mx-auto flex justify-between p-10">
+                    <div className="w-[300px] h-[180px] cursor-pointer rounded-lg bg-secondary-blue shadow-lg">
+                        <div className="p-5">
+                            <img className="w-[48px] h-[38px]" src={courseIcon.src}/>
+                            <p className="text-lg mt-2">Courses</p>
+                        </div>
+                        <div className="flex justify-end">
+                            <p className="text-2xl font-bold p-5 mr-5">999</p>
+                        </div>
+                    </div>
+                    <div className="w-[300px] h-[180px] cursor-pointer rounded-lg bg-secondary-blue shadow-lg">
+                        <div className="p-5">
+                            <img className="w-[48px] h-[38px]" src={studentIcon.src}/>
+                            <p className="text-lg mt-2">Students</p>
+                        </div>
+                        <div className="flex justify-end">
+                            <p className="text-2xl font-bold p-5 mr-5">999</p>
+                        </div>
+                    </div><div className="w-[300px] h-[180px] cursor-pointer rounded-lg bg-secondary-blue shadow-lg">
+                        <div className="p-5">
+                            <img className="w-[48px] h-[38px]" src={paymentIcon.src}/>
+                            <p className="text-lg mt-2">Payments</p>
+                        </div>
+                        <div className="flex justify-end">
+                            <p className="text-2xl font-bold p-5 mr-5">999</p>
+                        </div>
+                    </div><div className="w-[300px] h-[180px] cursor-pointer rounded-lg bg-secondary-blue shadow-lg">
+                        <div className="p-5">
+                            <img className="w-[48px] h-[38px]" src={reportIcon.src}/>
+                            <p className="text-lg mt-2">Reports</p>
+                        </div>
+                        <div className="flex justify-end">
+                            <p className="text-2xl font-bold p-5 mr-5">999</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
